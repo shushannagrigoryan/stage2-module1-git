b8f97b93465fd75c2e257cd8be90b21a4b146136
Shushanna Grigoryan <shush.grig.github@gmail.com>
2023-01-27 18:06:06 +0400

    2

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/shushannagrigoryan/first_repo.git";
+    public static String REPO_LINK = "https://github.com/shushannagrigoryan/repo.git";
 }
